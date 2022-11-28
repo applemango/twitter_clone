5957fa2855a9e2e42451e2b47cdc1e7706e9fb7f
applemango <85370582+applemango@users.noreply.github.com>
2022-11-28 11:40:04 +0900

    add user icon

--- page/pages/components/usericon.tsx
+++ page/pages/components/usericon.tsx
@@ -1,4 +1,21 @@
-const UserIcon = () => {
-    return <div></div>
+import styles from "./usericon.module.scss"
+const UserIcon = ({
+    width = 32,
+    hight = 32,
+    url = ""
+}:{
+    width?: number
+    hight?: number
+    url?: string
+}) => {
+    const loader = () => {
+        return
+    }
+    return <div>
+        { url ? <div style={{
+            width: width,
+            hight: hight
+        }} className={styles.usericonnone} /> : <div />}
+    </div>
 }
 export default UserIcon
